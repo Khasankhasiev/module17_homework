c21afd3ead43b23acc9f0016d820df2b4f9a3f87
Khasankhasiev <110396097+Khasankhasiev@users.noreply.github.com>
2023-05-08 22:19:30 +0300

    Update index.test.js
    add suit and change test

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,15 @@
-export function multiply(a, b) {
-    return a * b;
-  }
+import { multiply } from './index.js';
+
+describe(('test-multiply'), ()=> {
+
+    it('adds 1 * 0 to equal 1', () => {
+        const result = multiply(1, 0)
+        expect(result).toBe(0);
+    }),
+
+    xit('adds 1 * 1 to equal 1', () => {
+        const result = multiply(1, 1)
+        expect(result).toBe(1);
+    })
+
+});
